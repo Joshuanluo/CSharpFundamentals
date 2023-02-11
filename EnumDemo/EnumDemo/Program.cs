cb6e6b3e016b766a27d19407f4ce596657f4e691
Joshuanluo <joshua.chuan.luo@gmail.com>
2023-02-11 22:57:35 +1100

    enum demo

---
+++ EnumDemo/EnumDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EnumDemo;
+public enum ShippingMethod
+{
+    RegularAirMail=1,
+    RegisteredAirmail=2,
+    Express=3
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        var method = ShippingMethod.Express;
+        Console.WriteLine((int)method);
+
+        var methodId = 3;
+        Console.WriteLine((ShippingMethod)methodId);
+
+        Console.WriteLine(method.ToString());
+
+        var methodName = "Express";
+        var shipmethod=(ShippingMethod)Enum.Parse(typeof(ShippingMethod),methodName);
+        Console.WriteLine(shipmethod);
+    }
+}
+
